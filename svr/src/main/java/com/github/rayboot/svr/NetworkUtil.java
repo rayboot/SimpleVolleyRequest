d288516a99495e30905d1d3363486ef4b307fa38
rayboot <rayboot.work@gmail.com>
2015-11-04 09:47:01 +0800

    Update NetworkUtil.java
    修改后去整个url的方法

--- svr/src/main/java/com/github/rayboot/svr/NetworkUtil.java
+++ svr/src/main/java/com/github/rayboot/svr/NetworkUtil.java
@@ -3,10 +3,13 @@ package com.github.rayboot.svr;
 import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.net.Uri;
 
 import com.android.volley.NetworkResponse;
 
+import java.util.HashMap;
 import java.util.Map;
+
 
 /**
  * @author rayboot
@@ -32,14 +35,21 @@ public class NetworkUtil {
             return url;
         }
 
-        StringBuilder fullUrl = new StringBuilder();
-        fullUrl.append(url);
-        fullUrl.append("?");
-        for (Map.Entry entry : params.entrySet()) {
-            fullUrl.append(String.format("%s=%s&", entry.getKey(), entry.getValue()));
+//        StringBuilder fullUrl = new StringBuilder();
+//        fullUrl.append(url);
+//        fullUrl.append("?");
+//        for (Map.Entry entry : params.entrySet()) {
+//            fullUrl.append(String.format("%s=%s&", entry.getKey(), entry.getValue()));
+//        }
+//        fullUrl.deleteCharAt(fullUrl.length() - 1);
+//        return fullUrl.toString();
+
+        Uri uri = Uri.parse(url);
+        Uri.Builder builder = uri.buildUpon();
+        for (HashMap.Entry<String, String> entry : params.entrySet()) {
+            builder.appendQueryParameter(entry.getKey(), entry.getValue());
         }
-        fullUrl.deleteCharAt(fullUrl.length() - 1);
-        return fullUrl.toString();
+        return builder.build().toString();
     }
 
 
